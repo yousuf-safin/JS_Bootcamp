08578064719693be832c1706519889375982c482
yousuf-safin <yousufalisafin@gmail.com>
2025-03-01 23:42:42 +0600

    Module_1_Class_1

--- Module1/script.js
+++ Module1/script.js
@@ -6,3 +6,25 @@ var a = 10;
 var b = 20;
 var c = a + b;
 console.log(c);
+
+var name = "Yousuf Ali";
+console.log('name:', name);
+
+var strData = "abc";
+strData = '123';
+typeof strData;
+console.log('strData:', strData);
+
+console.log(typeof strData);
+var objectType = {
+    name: "Yousuf Ali",
+    age: 22,
+    isMarried: false,}
+console.log(objectType);
+objectType.father = {
+    name: "Ali",
+    age: 50,
+    isMarried: true,
+}
+console.log(objectType);
+console.log(objectType.father.name);
